28478d8d1e1bb3cee6cf6e5200f3b0a4d241d4d8
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-05-10 15:38:31 +0700

    them doYouLoveMe

---
+++ doYouLoveMe.js
@@ -0,0 +1,23 @@
+function thaTim(){
+    alert('<3');
+}
+function nhay1(){
+        document.getElementById('no1').style.display='none';
+        document.getElementById('no4').style.display='inline';
+}
+function nhay2(){
+    document.getElementById('no2').style.display='none';
+    document.getElementById('no1').style.display='inline';
+}
+function nhay3(){
+    document.getElementById('no3').style.display='none';
+    document.getElementById('no5').style.display='inline';
+}
+function nhay4(){
+    document.getElementById('no4').style.display='none';
+    document.getElementById('no3').style.display='inline';
+}
+function nhay5(){
+    document.getElementById('no5').style.display='none';
+    document.getElementById('no2').style.display='inline';
+}
